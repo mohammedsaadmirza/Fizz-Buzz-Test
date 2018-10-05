9f99497d5b3a79febca829df81e3559f116ebf70
mohammedsaadmirza <40477964+mohammedsaadmirza@users.noreply.github.com>
2018-10-04 21:23:57 -0700

    Add files via upload

---
+++ FizzBuzz Test/FizzBuzz Test/FizzBuzz Test.cpp
@@ -0,0 +1,28 @@
+// FizzBuzz Test.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	for (int i = 1; i <= 100; i++)
+	{
+		if (i % 15 == 0)
+			cout << "FizzBuzz" << endl;
+
+		else if (i % 5 == 0)
+			cout << "Buzz" << endl;
+
+		else if (i % 3 == 0)
+			cout << "Fizz" << endl;
+
+		else
+			cout << i << endl;
+
+	}
+
+    return 0;
+}
